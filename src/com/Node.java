ce5ec4d485af480dbf59fd3cf4c133a4ef2f7431
Sjoerd <sbeetsma99@gmail.com>
2021-05-27 19:58:43 +0200

    Add attributes to the Node class necessary to implement Dijkstra's shortest path Algo.

--- src/com/Node.java
+++ src/com/Node.java
@@ -1,27 +1,58 @@
 package com;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+// nodes for graph
+public class Node implements Comparable<Node> {
+    private String naam;
+    private Node voorganger;
+    private int AfstandVanafBegin = Integer.MAX_VALUE;
+    private boolean bezocht;
 
-public class Node {
-    final String label;
-    private Map<Node, Integer> neighbors = new HashMap<>();
-
-    public Node(String label) {
-        this.label = label;
+    public Node(String naam) {
+        this.naam = naam;
     }
 
-    public void addNeighbor(Node node, int distance){
-        this.neighbors.put(node, distance);
+    private ArrayList<Stap> overgangen = new ArrayList<>(); // alle overgangen
+
+    public int getAfstandVanafBegin() {
+        return AfstandVanafBegin;
     }
 
-    public Map<Node, Integer> getNeighbors(){
-        return this.neighbors;
+    public void setAfstandVanafBegin(int afstandVanafBegin) {
+        AfstandVanafBegin = afstandVanafBegin;
     }
 
+    public boolean isBezocht() {
+        return bezocht;
+    }
 
+    public void setBezocht(boolean bezocht) {
+        this.bezocht = bezocht;
+    }
 
+    public ArrayList<Stap> getOvergangen() {
+        return overgangen;
+    }
 
+    public void setOvergangen(ArrayList<Stap> overgangen) {
+        this.overgangen = overgangen;
+    }
 
+    public void setVoorganger(Node voorganger) {
+        this.voorganger = voorganger;
+    }
 
+    public Node getVoorganger() {
+        return voorganger;
+    }
+
+    @Override
+    public String toString() {
+        return this.naam;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return Integer.compare(this.AfstandVanafBegin, o.AfstandVanafBegin);
+    }
 }
