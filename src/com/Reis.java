3f36e0699f957967a975ef0350bb4a1cccaba8a7
Sjoerd <sbeetsma99@gmail.com>
2021-05-28 01:46:58 +0200

    Add method to get the list of the shortest path given a destination node. Can give any shortest distance from the used node in dijkstra. implement interface comparable. fix -> uppercase starting methods to lowercase

--- src/com/Reis.java
+++ src/com/Reis.java
@@ -2,35 +2,69 @@ package com;
 
 import java.util.*;
 
-public class Reis {
-    private final Node startNode;
-    private final Node eindNode;
+public class Reis implements Comparable<Reis> {
+    private Node startNode;
+    private Node eindNode;
     private ArrayList<Node> kortstePad;
+    private int totaalGewicht = 0;
 
     public Reis(Node startNode, Node eindNode){
         this.startNode = startNode;
         this.eindNode = eindNode;
+        // voor elke nieuwe reis wordt opnieuw uitgerekend voor elke node wat zijn kortste pad is;
+        dijkstra(startNode);
+        // totaal gewicht wordt in KorstePad ook geupdate
+        this.kortstePad = getKortstePad(eindNode);
+        this.totaalGewicht = eindNode.getAfstandVanafBegin();
     }
+    public ArrayList<Node> getKortstePad() {
+        return kortstePad;
+
+    }
+
+    public int getTotaalGewicht() {
+        return totaalGewicht;
+    }
+
     // leerbron: http://nmamano.com/blog/dijkstra/dijkstra.html
-    public void Dijkstra() {
-        this.startNode.setBezocht(true);
-        this.startNode.setAfstandVanafBegin(0);  // afstand van startpunt naar startpunt is 0
-        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(); // priorityQueue is gebasseerd op totaal gewicht/afstand attribuut van Node
-        priorityQueue.add(this.startNode);
-        while (!priorityQueue.isEmpty()) {
-            Node huidigeNode = priorityQueue.peek();
-            for (Stap overgang : huidigeNode.getOvergangen()) {
-                Node volgendeNode = overgang.getNaar();
-                if (!volgendeNode.isBezocht()) {
-                    int nieuweAfstand = huidigeNode.getAfstandVanafBegin() + overgang.getGewicht();
-                    if (nieuweAfstand < volgendeNode.getAfstandVanafBegin()) {
-                        volgendeNode.setAfstandVanafBegin(nieuweAfstand);
+    public void dijkstra(Node startPunt){
+        startPunt.setBezocht(true);
+        startPunt.setAfstandVanafBegin(0);  // afstand van startpunt naar startpunt is 0
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(); // priorityQueue gebasseerd op totaal gewicht/afstand attribuut van Node
+        priorityQueue.add(startPunt);
+        while (!priorityQueue.isEmpty()){
+            Node huidigeNode = priorityQueue.poll();
+            for(Stap overgang : huidigeNode.getOvergangen()){Node volgendeNode = overgang.getBestemming();
+                if (!volgendeNode.isBezocht()) {this.totaalGewicht = huidigeNode.getAfstandVanafBegin()+overgang.getGewicht();
+                    if(this.totaalGewicht<volgendeNode.getAfstandVanafBegin()){
+                        volgendeNode.setAfstandVanafBegin(this.totaalGewicht);
                         volgendeNode.setVoorganger(huidigeNode);
                         priorityQueue.add(volgendeNode);
                     }
                 }
+
             }
             priorityQueue.remove(huidigeNode);
             huidigeNode.setBezocht(true);
         }
-    }}
+    }
+
+        // method die het kortste pad returned vanaf een target node gebasseerd op de eerder uitgerekende,
+
+
+        public ArrayList<Node> getKortstePad(Node target){
+            ArrayList<Node> pad = new ArrayList<>(Collections.singletonList(target));
+
+            while (target.getVoorganger() != null) {
+                pad.add(target.getVoorganger());
+                target = Objects.requireNonNull(target).getVoorganger();
+            }
+    Collections.reverse(pad);
+    return pad;
+    }
+
+    @Override
+    public int compareTo(Reis o) {
+        return Integer.compare(this.totaalGewicht, o.totaalGewicht);
+    }
+}
