f31cd51c74a1162bfb49e915a86907cb5cc4256d
Sjoerd <sbeetsma99@gmail.com>
2021-05-28 16:06:12 +0200

    Change negative filter for extra transfers +50 instead of +100

--- src/com/Vlucht.java
+++ src/com/Vlucht.java
@@ -2,7 +2,7 @@ package com;
 
 public class Vlucht extends Stap{
     public Vlucht(Node naar, double prijs) {
-        // door +100 bij het gewicht is het negatieve filter om routes met veel overgangen extra nadeel te geven (bagage verlies)
-        super(naar, prijs+100);
+        // door +50 bij het gewicht is het negatieve filter om routes met veel overgangen extra nadeel te geven (bagage verlies)
+        super(naar, prijs+50);
     }
 }
